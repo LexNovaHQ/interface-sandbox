143bea2a51a646040a1ed8ca48d074a42a1bfe65
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-06-06 16:25:19 +0530

    Add Assembly intake exports

---
+++ src/wrapper/assembly/intake/index.js
@@ -0,0 +1,7 @@
+export { default as AssemblyIntakePanel } from "./AssemblyIntakePanel.jsx";
+
+export {
+  assertAssemblyIntakeReady,
+  createAssemblyIntake,
+  validateAssemblyIntake
+} from "./assemblyIntake.js";
