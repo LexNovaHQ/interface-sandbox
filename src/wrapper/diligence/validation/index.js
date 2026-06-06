d09777c85e8bdf48265ab8fda2367e050ae92f36
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-06-06 14:27:29 +0530

    Add validation utility index

---
+++ src/wrapper/diligence/validation/index.js
@@ -0,0 +1,13 @@
+export {
+  clearSchemaCache,
+  getSchemaPath,
+  loadSchema
+} from "./schemaLoader.js";
+
+export {
+  assertValidAgainstSchema,
+  createValidator,
+  formatSchemaErrors,
+  validateAgainstSchema,
+  validateJsonSchema
+} from "./schemaValidator.js";
