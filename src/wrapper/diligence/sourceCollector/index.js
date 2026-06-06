dd5be013a04a7c2c9694e7d0d2fb4a3fb0403980
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-06-06 15:24:36 +0530

    Add source collector exports

---
+++ src/wrapper/diligence/sourceCollector/index.js
@@ -0,0 +1,14 @@
+export {
+  createJinaReaderUrl,
+  fetchWithJinaReader
+} from "./jinaClient.js";
+
+export {
+  SOURCE_MODES,
+  normalizeHttpUrl,
+  normalizeManualUrls,
+  normalizePastedText,
+  normalizeSourceInput
+} from "./sourceMode.js";
+
+export { collectDiligenceSources } from "./sourceCollector.js";
