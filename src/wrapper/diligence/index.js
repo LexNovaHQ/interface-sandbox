0902405b4dd415c89bb7af3afa8277dbbe8d8ee6
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-06-06 16:07:54 +0530

    Export Node 5B assembler

--- src/wrapper/diligence/index.js
+++ src/wrapper/diligence/index.js
@@ -26,3 +26,15 @@ export {
   validateRegistryBatchResult,
   validateRegistryRows
 } from "./registry/index.js";
+
+export {
+  assembleNode5B,
+  assertNode5BAssembled,
+  FORBIDDEN_NODE5B_INPUT_FIELDS,
+  getVaultGroup,
+  isAllowedVaultPath,
+  normalizeVaultFieldPath,
+  VAULT_FIELD_PATHS,
+  VAULT_FIELD_PATH_SET,
+  VAULT_GROUPS
+} from "./node5b/index.js";
