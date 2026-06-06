d811110f6fb1303dada9714d36a52bd832cdd672
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-06-06 16:07:35 +0530

    Add Node 5B exports

---
+++ src/wrapper/diligence/node5b/index.js
@@ -0,0 +1,14 @@
+export {
+  assembleNode5B,
+  assertNode5BAssembled
+} from "./node5bAssembler.js";
+
+export {
+  FORBIDDEN_NODE5B_INPUT_FIELDS,
+  VAULT_FIELD_PATHS,
+  VAULT_FIELD_PATH_SET,
+  VAULT_GROUPS,
+  getVaultGroup,
+  isAllowedVaultPath,
+  normalizeVaultFieldPath
+} from "./vaultAllowlist.js";
