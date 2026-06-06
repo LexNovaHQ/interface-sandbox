8d99cc1a95285ea27f010e5c02c4aad0f0e31615
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-06-06 20:46:42 +0530

    Attach normalizer to Final Compiler endpoint

--- functions/api/diligence-final-compiler.js
+++ functions/api/diligence-final-compiler.js
@@ -1,9 +1,11 @@
 import { handleDiligenceStageRequest } from "../_shared/diligenceStageHandler.js";
+import { normalizeFinalCompilerOutput } from "../_shared/finalCompilerNormalizer.js";
 
 export async function onRequest(context) {
   return handleDiligenceStageRequest(context, {
     stageId: "final_compiler",
     outputSchemaKey: "diligence_compiler_output",
-    outputKey: "compiler_output"
+    outputKey: "compiler_output",
+    normalizeOutput: normalizeFinalCompilerOutput
   });
 }
