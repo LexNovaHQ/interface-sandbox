b9eebfd4ae69fb1db909fed17498aad406efb688
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-06-12 07:57:12 +0530

    Use locked final compiler normalizer in API route

--- functions/api/diligence-final-compiler.js
+++ functions/api/diligence-final-compiler.js
@@ -1,11 +1,11 @@
 import { handleDiligenceStageRequest } from "../_shared/diligenceStageHandler.js";
-import { normalizeFinalCompilerForSchema } from "../_shared/finalCompilerSchemaGuard.js";
+import { normalizeFinalCompilerOutput } from "../_shared/finalCompilerNormalizer.js";
 
 export async function onRequest(context) {
   return handleDiligenceStageRequest(context, {
     stageId: "final_compiler",
     outputSchemaKey: "diligence_compiler_output",
     outputKey: "compiler_output",
-    normalizeOutput: normalizeFinalCompilerForSchema
+    normalizeOutput: normalizeFinalCompilerOutput
   });
 }
