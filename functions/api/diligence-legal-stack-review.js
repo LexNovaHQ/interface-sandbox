1cf6d38c81a2460df46d10ceee19cd7fbd65439c
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-06-14 05:42:55 +0530

    Delete legacy legal stack review API route

--- functions/api/diligence-legal-stack-review.js
+++
@@ -1,9 +0,0 @@
-import { handleDiligenceStageRequest } from "../_shared/diligenceStageHandler.js";
-
-export async function onRequest(context) {
-  return handleDiligenceStageRequest(context, {
-    stageId: "legal_stack_review",
-    outputSchemaKey: "legal_stack_review",
-    outputKey: "legal_stack_review"
-  });
-}
