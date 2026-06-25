870c6c431dd5c9cb04fee8ecabdf0f32249d782d
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-06-25 11:33:26 +0530

    Add Agent 2 forensics artifacts to permissions

--- diligence-artifact-backend/src/constants.js
+++ diligence-artifact-backend/src/constants.js
@@ -23,7 +23,9 @@ export const ARTIFACT_NAMES = Object.freeze([
   "source_discovery_handoff",
   "legal_cartography_index",
   "target_profile",
+  "target_profile_forensics",
   "target_feature_profile",
+  "target_feature_profile_forensics",
   "data_provenance_profile",
   "exposure_registry_profile",
   "challenge_gate",
@@ -45,7 +47,7 @@ export const AGENTS = Object.freeze([
 
 export const WRITE_PERMISSIONS = Object.freeze({
   agent_1_source_legal: ["source_discovery_handoff", "legal_cartography_index"],
-  agent_2_target_feature: ["target_profile", "target_feature_profile"],
+  agent_2_target_feature: ["target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics"],
   agent_3_data_privacy: ["data_provenance_profile"],
   agent_4_exposure_registry: ["exposure_registry_profile"],
   agent_5_challenge_assembly: ["challenge_gate", "final_output_handoff"],
@@ -56,10 +58,10 @@ export const WRITE_PERMISSIONS = Object.freeze({
 export const READ_PERMISSIONS = Object.freeze({
   agent_1_source_legal: [],
   agent_2_target_feature: ["source_discovery_handoff", "legal_cartography_index"],
-  agent_3_data_privacy: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_feature_profile"],
-  agent_4_exposure_registry: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_feature_profile", "data_provenance_profile"],
-  agent_5_challenge_assembly: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_feature_profile", "data_provenance_profile", "exposure_registry_profile"],
-  agent_6_terminal_validator: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_feature_profile", "data_provenance_profile", "exposure_registry_profile", "challenge_gate", "final_output_handoff"],
+  agent_3_data_privacy: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics"],
+  agent_4_exposure_registry: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics", "data_provenance_profile"],
+  agent_5_challenge_assembly: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics", "data_provenance_profile", "exposure_registry_profile"],
+  agent_6_terminal_validator: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics", "data_provenance_profile", "exposure_registry_profile", "challenge_gate", "final_output_handoff"],
   operator: ARTIFACT_NAMES,
   portfolio_renderer: ["renderer_payload"]
 });
