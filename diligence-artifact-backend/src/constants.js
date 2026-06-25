71c7e59c7b3eb309f3544805c55438c592b2940f
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-06-25 13:30:20 +0530

    Lock reviewer runner phases and artifact permissions

--- diligence-artifact-backend/src/constants.js
+++ diligence-artifact-backend/src/constants.js
@@ -1,12 +1,16 @@
 export const SERVICE_NAME = "interface-diligence-artifacts";
 
 export const PHASES = Object.freeze([
+  "URL_MANIFEST",
+  "LOSSLESS_SOURCE_EXTRACTION",
   "M6_M9",
   "M7_M8",
   "M10",
   "M11",
-  "M12_M13",
-  "M14"
+  "M12",
+  "COMPILER",
+  "RENDERER",
+  "COMPLETE"
 ]);
 
 export const LOCK_STATUSES = Object.freeze([
@@ -20,6 +24,8 @@ export const LOCK_STATUSES = Object.freeze([
 ]);
 
 export const ARTIFACT_NAMES = Object.freeze([
+  "url_manifest",
+  "lossless_source_corpus",
   "source_discovery_handoff",
   "legal_cartography_index",
   "target_profile",
@@ -30,40 +36,43 @@ export const ARTIFACT_NAMES = Object.freeze([
   "exposure_registry_profile",
   "challenge_gate",
   "final_output_handoff",
-  "terminal_validation",
   "renderer_payload"
 ]);
 
 export const AGENTS = Object.freeze([
-  "agent_1_source_legal",
-  "agent_2_target_feature",
-  "agent_3_data_privacy",
-  "agent_4_exposure_registry",
-  "agent_5_challenge_assembly",
-  "agent_6_terminal_validator",
-  "operator",
-  "portfolio_renderer"
+  "agent_1_m6_m9",
+  "deterministic_source_extractor",
+  "agent_2_m7_m8",
+  "agent_3_m10",
+  "agent_4_m11",
+  "agent_5_m12",
+  "compiler",
+  "portfolio_renderer",
+  "operator"
 ]);
 
 export const WRITE_PERMISSIONS = Object.freeze({
-  agent_1_source_legal: ["source_discovery_handoff", "legal_cartography_index"],
-  agent_2_target_feature: ["target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics"],
-  agent_3_data_privacy: ["data_provenance_profile"],
-  agent_4_exposure_registry: ["exposure_registry_profile"],
-  agent_5_challenge_assembly: ["challenge_gate", "final_output_handoff"],
-  agent_6_terminal_validator: ["terminal_validation", "renderer_payload"],
+  agent_1_m6_m9: ["url_manifest", "source_discovery_handoff", "legal_cartography_index"],
+  deterministic_source_extractor: ["lossless_source_corpus"],
+  agent_2_m7_m8: ["target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics"],
+  agent_3_m10: ["data_provenance_profile"],
+  agent_4_m11: ["exposure_registry_profile"],
+  agent_5_m12: ["challenge_gate"],
+  compiler: ["final_output_handoff"],
+  portfolio_renderer: ["renderer_payload"],
   operator: ARTIFACT_NAMES
 });
 
 export const READ_PERMISSIONS = Object.freeze({
-  agent_1_source_legal: [],
-  agent_2_target_feature: ["source_discovery_handoff", "legal_cartography_index"],
-  agent_3_data_privacy: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics"],
-  agent_4_exposure_registry: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics", "data_provenance_profile"],
-  agent_5_challenge_assembly: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics", "data_provenance_profile", "exposure_registry_profile"],
-  agent_6_terminal_validator: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics", "data_provenance_profile", "exposure_registry_profile", "challenge_gate", "final_output_handoff"],
-  operator: ARTIFACT_NAMES,
-  portfolio_renderer: ["renderer_payload"]
+  agent_1_m6_m9: ["url_manifest", "lossless_source_corpus"],
+  deterministic_source_extractor: ["url_manifest"],
+  agent_2_m7_m8: ["url_manifest", "lossless_source_corpus", "source_discovery_handoff", "legal_cartography_index"],
+  agent_3_m10: ["url_manifest", "lossless_source_corpus", "source_discovery_handoff", "legal_cartography_index", "target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics"],
+  agent_4_m11: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics", "data_provenance_profile"],
+  agent_5_m12: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics", "data_provenance_profile", "exposure_registry_profile"],
+  compiler: ["source_discovery_handoff", "legal_cartography_index", "target_profile", "target_profile_forensics", "target_feature_profile", "target_feature_profile_forensics", "data_provenance_profile", "exposure_registry_profile", "challenge_gate"],
+  portfolio_renderer: ["final_output_handoff", "renderer_payload"],
+  operator: ARTIFACT_NAMES
 });
 
 export function assertKnownArtifactName(artifactName) {
