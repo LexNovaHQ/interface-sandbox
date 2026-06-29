6a5eff69a19f8e387663f697b0efa8996d051d5e
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-06-30 03:04:29 +0530

    Use status helper in normalized compiler output

--- diligence-artifact-backend/src/compiler.js
+++ diligence-artifact-backend/src/compiler.js
@@ -1,20 +1,25 @@
 import { buildNormalizedProfilerOutput, NORMALIZED_SECTION_KEYS } from "./normalized-profiler.js";
+import { toMachineStatus } from "./normalized-status.js";
 
 export function compileFinalOutputHandoff({ run, artifacts }) {
   const output = buildNormalizedProfilerOutput({ run, artifacts });
   const final = output.final_output_handoff?.final_output_handoff || {};
+  const status = toMachineStatus(final.validation_status || output.normalized_report_manifest?.validation_status || run?.status);
   const normalized_sections = Object.fromEntries(
     NORMALIZED_SECTION_KEYS.map((sectionId) => [sectionId, output[`normalized_section__${sectionId}`]])
   );
 
+  output.normalized_report_manifest = { ...(output.normalized_report_manifest || {}), validation_status: status };
+  output.vault_section_handoff = { ...(output.vault_section_handoff || {}), validation_status: status };
+
   output.final_output_handoff = {
-    validation_status: "LOCKED_WITH_LIMITATIONS",
+    validation_status: status,
     normalized_report_manifest_ref: "normalized_report_manifest",
     vault_section_handoff_ref: "vault_section_handoff",
     section_artifacts: output.normalized_report_manifest?.section_artifacts || [],
     final_output_handoff: {
       ...final,
-      validation_status: "LOCKED_WITH_LIMITATIONS",
+      validation_status: status,
       normalized_report_manifest: output.normalized_report_manifest,
       vault_section_handoff: output.vault_section_handoff,
       normalized_sections,
