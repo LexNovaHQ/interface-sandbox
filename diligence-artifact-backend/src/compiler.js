db49b355e7c31c73d4f238bc52937f30ceffaa91
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-06-30 02:57:30 +0530

    Expose normalized compiler status for runner compatibility

--- diligence-artifact-backend/src/compiler.js
+++ diligence-artifact-backend/src/compiler.js
@@ -3,25 +3,23 @@ import { buildNormalizedProfilerOutput, NORMALIZED_SECTION_KEYS } from "./normal
 export function compileFinalOutputHandoff({ run, artifacts }) {
   const output = buildNormalizedProfilerOutput({ run, artifacts });
   const final = output.final_output_handoff?.final_output_handoff || {};
+  const normalized_sections = Object.fromEntries(
+    NORMALIZED_SECTION_KEYS.map((sectionId) => [sectionId, output[`normalized_section__${sectionId}`]])
+  );
 
   output.final_output_handoff = {
+    validation_status: final.validation_status,
+    normalized_report_manifest_ref: "normalized_report_manifest",
+    vault_section_handoff_ref: "vault_section_handoff",
+    section_artifacts: output.normalized_report_manifest?.section_artifacts || [],
     final_output_handoff: {
       ...final,
       normalized_report_manifest: output.normalized_report_manifest,
       vault_section_handoff: output.vault_section_handoff,
-      normalized_sections: Object.fromEntries(
-        NORMALIZED_SECTION_KEYS.map((sectionId) => [
-          sectionId,
-          output[`normalized_section__${sectionId}`]
-        ])
-      ),
+      normalized_sections,
       compiler_trace: {
         compiler_version: "normalized_profiler_compiler_replacement_v1",
         deterministic_only: true,
-        no_new_findings_created: true,
-        no_row_re_evaluation: true,
-        legacy_profiles_combined_retired: true,
-        legacy_forensics_combined_retired: true,
         normalized_section_count: NORMALIZED_SECTION_KEYS.length
       }
     }
