33aa9bbb1a568eb8fe463a4428893c9f57a402ef
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-01 00:39:42 +0530

    Export Qualified Review branch builder

--- diligence-artifact-backend/src/qualified-review-system/index.js
+++ diligence-artifact-backend/src/qualified-review-system/index.js
@@ -1,3 +1,4 @@
+export { buildQualifiedReviewSystemArtifacts } from "./branch.js";
 export { buildQualifiedReviewHandoff } from "./handoff.js";
 export { buildQualifiedReviewQuestionHandoff, QUALIFIED_REVIEW_QUESTIONS, QUALIFIED_REVIEW_SECTIONS } from "./question-map.js";
 export { validateQualifiedReviewQuestionHandoff } from "./qr-validator.js";
