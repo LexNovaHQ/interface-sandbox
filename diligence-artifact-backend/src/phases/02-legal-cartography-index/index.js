b738d0d5e77182b3d62e9c749db116f49c408e23
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-05 05:23:53 +0530

    Export Phase 2 legal signal derivation modules

---
+++ diligence-artifact-backend/src/phases/02-legal-cartography-index/index.js
@@ -0,0 +1,4 @@
+export { LEGAL_CARTOGRAPHY_INDEX_PHASE } from "./legal-cartography-index.phase.js";
+export { LEGAL_CARTOGRAPHY_INDEX_CONTRACT, LEGAL_SIGNAL_DERIVATION_PROFILE_CONTRACT, requiredLegalSignalFieldRows } from "./legal-cartography-index.contract.js";
+export { LEGAL_SIGNAL_DERIVATION_JOB, buildLegalSignalDerivation } from "./jobs/legal-signal-derivation.job.js";
+export { buildLegalSignalDerivationProfile } from "./services/legal-signal-derivation.service.js";
