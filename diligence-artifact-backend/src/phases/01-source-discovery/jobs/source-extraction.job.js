7bb3e85797effb62e8fc180c994d3946b7508e88
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-04 22:38:30 +0530

    Use phase-owned source extraction service

--- diligence-artifact-backend/src/phases/01-source-discovery/jobs/source-extraction.job.js
+++ diligence-artifact-backend/src/phases/01-source-discovery/jobs/source-extraction.job.js
@@ -1,16 +1,16 @@
-import { buildAgent1bExtractArtifacts as buildLegacySourceExtractionArtifacts } from "../../../agent-1-scout-extractor.js";
+import { buildSourceExtractionArtifactSet } from "../services/source-extraction.service.js";
 import { assertSourceDiscoveryBoundary, assertNoSourceDiscoveryModelUsage } from "../validators/source-discovery.validator.js";
 
 export const SOURCE_DISCOVERY_EXTRACTION_JOB = Object.freeze({
   phase_id: "SOURCE_DISCOVERY",
   job_id: "SOURCE_EXTRACTION",
   public_label: "Source Extraction",
-  implementation_status: "WRAPS_EXISTING_HELPER",
+  implementation_status: "PHASE_OWNED_IMPLEMENTATION",
   model_usage: "NONE"
 });
 
 export async function buildSourceExtractionArtifacts({ run, deduped_url_manifest } = {}) {
-  const output = await buildLegacySourceExtractionArtifacts({ run, deduped_url_manifest });
+  const output = await buildSourceExtractionArtifactSet({ run, deduped_url_manifest });
   assertNoSourceDiscoveryModelUsage({ job_id: SOURCE_DISCOVERY_EXTRACTION_JOB.job_id });
   assertSourceDiscoveryBoundary({ job_id: SOURCE_DISCOVERY_EXTRACTION_JOB.job_id, output });
   return output;
