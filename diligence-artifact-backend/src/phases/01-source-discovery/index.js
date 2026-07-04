9287a7718338017e8d19fede1222a40791b94d4a
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-04 22:12:07 +0530

    Add source discovery phase index

---
+++ diligence-artifact-backend/src/phases/01-source-discovery/index.js
@@ -0,0 +1,7 @@
+export { SOURCE_DISCOVERY_PHASE } from "./source-discovery.phase.js";
+export { SOURCE_DISCOVERY_CONTRACT, getSourceDiscoveryJobContract } from "./source-discovery.contract.js";
+export { SOURCE_DISCOVERY_RUNNER, runSourceDiscoveryJob, runSourceUrlManifestJob, runSourceExtractionJob, runSourceFamilyHandoffJob } from "./source-discovery.runner.js";
+export { SOURCE_DISCOVERY_URL_MANIFEST_JOB, buildSourceUrlManifest } from "./jobs/url-manifest.job.js";
+export { SOURCE_DISCOVERY_EXTRACTION_JOB, buildSourceExtractionArtifacts } from "./jobs/source-extraction.job.js";
+export { SOURCE_DISCOVERY_FAMILY_HANDOFF_JOB, buildSourceFamilyHandoff } from "./jobs/source-family-handoff.job.js";
+export { assertSourceDiscoveryBoundary, assertNoSourceDiscoveryModelUsage } from "./validators/source-discovery.validator.js";
