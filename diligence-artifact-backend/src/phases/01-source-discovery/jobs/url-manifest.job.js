c0eab1aa9615c6506d1ae771f6f9c5f9d66405a3
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-04 22:30:35 +0530

    Use phase-owned URL manifest service

--- diligence-artifact-backend/src/phases/01-source-discovery/jobs/url-manifest.job.js
+++ diligence-artifact-backend/src/phases/01-source-discovery/jobs/url-manifest.job.js
@@ -1,16 +1,16 @@
-import { buildAgent1aDedupedUrlManifest as buildLegacyUrlManifest } from "../../../agent-1-scout-extractor.js";
+import { buildSourceUrlManifestArtifact } from "../services/url-manifest.service.js";
 import { assertSourceDiscoveryBoundary, assertNoSourceDiscoveryModelUsage } from "../validators/source-discovery.validator.js";
 
 export const SOURCE_DISCOVERY_URL_MANIFEST_JOB = Object.freeze({
   phase_id: "SOURCE_DISCOVERY",
   job_id: "URL_MANIFEST",
   public_label: "Source URL Manifest",
-  implementation_status: "WRAPS_EXISTING_HELPER",
+  implementation_status: "PHASE_OWNED_IMPLEMENTATION",
   model_usage: "NONE"
 });
 
 export async function buildSourceUrlManifest({ run } = {}) {
-  const output = await buildLegacyUrlManifest({ run });
+  const output = await buildSourceUrlManifestArtifact({ run });
   assertNoSourceDiscoveryModelUsage({ job_id: SOURCE_DISCOVERY_URL_MANIFEST_JOB.job_id });
   assertSourceDiscoveryBoundary({ job_id: SOURCE_DISCOVERY_URL_MANIFEST_JOB.job_id, output });
   return output;
