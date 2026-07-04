c151b595f52532ccd5fd087fd79cfe3a7ab72c6d
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-04 22:39:45 +0530

    Export phase-owned source extraction service

--- diligence-artifact-backend/src/phases/01-source-discovery/index.js
+++ diligence-artifact-backend/src/phases/01-source-discovery/index.js
@@ -4,5 +4,6 @@ export { SOURCE_DISCOVERY_RUNNER, runSourceDiscoveryJob, runSourceUrlManifestJob
 export { SOURCE_DISCOVERY_URL_MANIFEST_JOB, buildSourceUrlManifest } from "./jobs/url-manifest.job.js";
 export { buildSourceUrlManifestArtifact } from "./services/url-manifest.service.js";
 export { SOURCE_DISCOVERY_EXTRACTION_JOB, buildSourceExtractionArtifacts } from "./jobs/source-extraction.job.js";
+export { buildSourceExtractionArtifactSet } from "./services/source-extraction.service.js";
 export { SOURCE_DISCOVERY_FAMILY_HANDOFF_JOB, buildSourceFamilyHandoff } from "./jobs/source-family-handoff.job.js";
 export { assertSourceDiscoveryBoundary, assertNoSourceDiscoveryModelUsage } from "./validators/source-discovery.validator.js";
