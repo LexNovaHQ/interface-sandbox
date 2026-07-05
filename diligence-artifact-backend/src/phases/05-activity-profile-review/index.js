7dd1d185b88f104e995806d48b30aaa737af19f0
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-05 12:57:58 +0530

    Export Activity Profile Review phase modules

---
+++ diligence-artifact-backend/src/phases/05-activity-profile-review/index.js
@@ -0,0 +1,2 @@
+export { ACTIVITY_PROFILE_REVIEW_PHASE } from "./activity-profile-review.phase.js";
+export { ACTIVITY_CANDIDATE_INVENTORY_CONTRACT, activityCandidateInventoryReadArtifacts, activityCandidateInventoryWriteArtifacts, activityCandidateInventoryCandidateCreationFamilies } from "./activity-candidate-inventory.contract.js";
