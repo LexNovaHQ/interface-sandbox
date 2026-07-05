e285e9da40f762f3d91faa3e0254905abec91010
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-05 11:25:09 +0530

    Export Target Profile Forensics phase modules

---
+++ diligence-artifact-backend/src/phases/04-target-profile-forensics/index.js
@@ -0,0 +1,3 @@
+export { TARGET_PROFILE_FORENSICS_PHASE } from "./target-profile-forensics.phase.js";
+export { TARGET_PROFILE_FORENSICS_CONTRACT, targetProfileForensicsReadArtifacts, targetProfileForensicsWriteArtifacts } from "./target-profile-forensics.contract.js";
+export { TARGET_PROFILE_FORENSICS_RUNNER_STATUS, runTargetProfileForensicsPhase } from "./target-profile-forensics.runner.js";
