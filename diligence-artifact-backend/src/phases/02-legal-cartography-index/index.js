42f535a121a501c20b19a7b87721b24973c8de8b
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-05 05:48:55 +0530

    Export Phase 2 legal cartography runner

--- diligence-artifact-backend/src/phases/02-legal-cartography-index/index.js
+++ diligence-artifact-backend/src/phases/02-legal-cartography-index/index.js
@@ -1,5 +1,6 @@
 export { LEGAL_CARTOGRAPHY_INDEX_PHASE } from "./legal-cartography-index.phase.js";
 export { LEGAL_CARTOGRAPHY_INDEX_CONTRACT, LEGAL_SIGNAL_DERIVATION_PROFILE_CONTRACT, requiredLegalSignalFieldRows } from "./legal-cartography-index.contract.js";
+export { LEGAL_CARTOGRAPHY_INDEX_RUNNER, runLegalCartographyPhaseJob } from "./legal-cartography-index.runner.js";
 export { LEGAL_SIGNAL_DERIVATION_JOB, buildLegalSignalDerivation } from "./jobs/legal-signal-derivation.job.js";
 export { buildLegalSignalDerivationProfile } from "./services/legal-signal-derivation.service.js";
 export { assertLegalSignalDerivationProfile, validateLegalSignalDerivationProfile } from "./validators/legal-signal-derivation.validator.js";
