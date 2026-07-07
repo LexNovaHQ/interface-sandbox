c94a0ecf52dd946605df553ce7c165dc053ba1b2
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-07 09:55:05 +0530

    Export Phase 7 data privacy package foundation

---
+++ diligence-artifact-backend/src/phases/07-data-provenance-profile/index.js
@@ -0,0 +1,10 @@
+export { DATA_PROVENANCE_PROFILE_PHASE } from "./data-provenance-profile.phase.js";
+export { PHASE7_DATA_PRIVACY_ARCHITECTURE_CONTRACT, PHASE7_LAYER1_ARTIFACTS } from "./data-provenance-profile.contract.js";
+export {
+  PHASE7_DAP_MATERIAL_SECTION_MATRIX,
+  PHASE7_EXPECTED_DAP_FIELD_COUNT,
+  PHASE7_MODEL_PACKET_MATRIX,
+  PHASE7_REGISTRY_SOURCE_PATH,
+  compilePhase7DapRegistryDerivationRules,
+  validatePhase7DapRegistryManifest
+} from "./dap-registry-derivation-rule-compiler.js";
