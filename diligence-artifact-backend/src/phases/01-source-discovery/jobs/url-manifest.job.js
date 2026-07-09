6f8c0703958b7e3697924f8dacbed0e87cbf33e1
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-09 09:22:10 +0530

    Wire Phase 1 discovery control artifacts

--- diligence-artifact-backend/src/phases/01-source-discovery/jobs/url-manifest.job.js
+++ diligence-artifact-backend/src/phases/01-source-discovery/jobs/url-manifest.job.js
@@ -1,17 +1,20 @@
 import { buildSourceUrlManifestArtifact } from "../services/url-manifest.service.js";
+import { buildPhase1UrlManifestUpgradeArtifacts } from "../services/phase1-agnostic-upgrade.service.js";
 import { assertSourceDiscoveryBoundary, assertNoSourceDiscoveryModelUsage } from "../validators/source-discovery.validator.js";
 
 export const SOURCE_DISCOVERY_URL_MANIFEST_JOB = Object.freeze({
   phase_id: "SOURCE_DISCOVERY",
   job_id: "URL_MANIFEST",
   public_label: "Source URL Manifest",
-  implementation_status: "PHASE_OWNED_IMPLEMENTATION",
+  implementation_status: "PHASE_OWNED_IMPLEMENTATION_AGNOSTIC_UPGRADE_V0",
   model_usage: "NONE"
 });
 
-export async function buildSourceUrlManifest({ run } = {}) {
-  const output = await buildSourceUrlManifestArtifact({ run });
+export async function buildSourceUrlManifest({ run, preflight_context } = {}) {
+  const output = await buildSourceUrlManifestArtifact({ run, preflightContext: preflight_context });
+  const upgrade = buildPhase1UrlManifestUpgradeArtifacts({ run, preflight: preflight_context, deduped_url_manifest: output.deduped_url_manifest });
+  const merged = { ...output, ...upgrade };
   assertNoSourceDiscoveryModelUsage({ job_id: SOURCE_DISCOVERY_URL_MANIFEST_JOB.job_id });
-  assertSourceDiscoveryBoundary({ job_id: SOURCE_DISCOVERY_URL_MANIFEST_JOB.job_id, output });
-  return output;
+  assertSourceDiscoveryBoundary({ job_id: SOURCE_DISCOVERY_URL_MANIFEST_JOB.job_id, output: merged });
+  return merged;
 }
