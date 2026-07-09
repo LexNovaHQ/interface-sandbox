56dd29d9a535dc83e53a78e689e19545602a66b9
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-09 19:07:36 +0530

    Add Phase 3B index exports

---
+++ diligence-artifact-backend/src/phases/03-domain-derivation/index.js
@@ -0,0 +1,3 @@
+export { DOMAIN_DERIVATION_CONTRACT, domainDerivationReadArtifacts, domainDerivationLosslessReadArtifacts } from "./domain-derivation.contract.js";
+export { DOMAIN_DERIVATION_RUNNER_STATUS, runDomainDerivationPhase } from "./domain-derivation.runner.js";
+export { assertDomainDerivationRuntimeArtifacts, compileDomainDerivationArtifacts, validateDomainDerivationProfile } from "./validators/domain-derivation.validator.js";
