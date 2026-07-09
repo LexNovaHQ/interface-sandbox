580829eeea541eb909117b72e60d7b8078631620
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-09 09:38:38 +0530

    Remove Phase 1 compatibility upgrade wrapper from URL job

--- diligence-artifact-backend/src/phases/01-source-discovery/jobs/url-manifest.job.js
+++ diligence-artifact-backend/src/phases/01-source-discovery/jobs/url-manifest.job.js
@@ -1,20 +1,17 @@
 import { buildSourceUrlManifestArtifact } from "../services/url-manifest.service.js";
-import { buildPhase1UrlManifestUpgradeArtifacts } from "../services/phase1-agnostic-upgrade.service.js";
 import { assertSourceDiscoveryBoundary, assertNoSourceDiscoveryModelUsage } from "../validators/source-discovery.validator.js";
 
 export const SOURCE_DISCOVERY_URL_MANIFEST_JOB = Object.freeze({
   phase_id: "SOURCE_DISCOVERY",
   job_id: "URL_MANIFEST",
   public_label: "Source URL Manifest",
-  implementation_status: "PHASE_OWNED_IMPLEMENTATION_AGNOSTIC_UPGRADE_V0",
+  implementation_status: "PHASE_OWNED_IMPLEMENTATION_AGNOSTIC_V1",
   model_usage: "NONE"
 });
 
 export async function buildSourceUrlManifest({ run, preflight_context } = {}) {
   const output = await buildSourceUrlManifestArtifact({ run, preflightContext: preflight_context });
-  const upgrade = buildPhase1UrlManifestUpgradeArtifacts({ run, preflight: preflight_context, deduped_url_manifest: output.deduped_url_manifest });
-  const merged = { ...output, ...upgrade };
   assertNoSourceDiscoveryModelUsage({ job_id: SOURCE_DISCOVERY_URL_MANIFEST_JOB.job_id });
-  assertSourceDiscoveryBoundary({ job_id: SOURCE_DISCOVERY_URL_MANIFEST_JOB.job_id, output: merged });
-  return merged;
+  assertSourceDiscoveryBoundary({ job_id: SOURCE_DISCOVERY_URL_MANIFEST_JOB.job_id, output });
+  return output;
 }
