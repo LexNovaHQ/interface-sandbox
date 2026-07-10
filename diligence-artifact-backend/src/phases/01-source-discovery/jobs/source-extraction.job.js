2c59534094596817a61e3b0cba4b6cb27b252860
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-10 07:11:05 +0530

    Sync Phase 1 extraction metadata to V4

--- diligence-artifact-backend/src/phases/01-source-discovery/jobs/source-extraction.job.js
+++ diligence-artifact-backend/src/phases/01-source-discovery/jobs/source-extraction.job.js
@@ -1,11 +1,12 @@
 import { buildSourceExtractionArtifactSet } from "../services/source-extraction.service.js";
+import { SOURCE_DISCOVERY_CONTRACT } from "../source-discovery.contract.js";
 import { assertSourceDiscoveryBoundary, assertNoSourceDiscoveryModelUsage } from "../validators/source-discovery.validator.js";
 
 export const SOURCE_DISCOVERY_EXTRACTION_JOB = Object.freeze({
   phase_id: "SOURCE_DISCOVERY",
   job_id: "SOURCE_EXTRACTION",
   public_label: "Source Extraction",
-  implementation_status: "PHASE_OWNED_IMPLEMENTATION_AGNOSTIC_V1",
+  implementation_status: SOURCE_DISCOVERY_CONTRACT.implementation_status,
   model_usage: "NONE"
 });
 
