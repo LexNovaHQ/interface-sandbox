23efecdc675db9f83ed8039e550e4c1dbbd5bd91
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-12 20:44:45 +0530

    Expose Phase 12 route admission projection adapters

---
+++ diligence-artifact-backend/src/phases/12-normalized-compiler/phase12-adapters.js
@@ -0,0 +1,4 @@
+export { buildPhase12AdmissionAdapter, assertPhase12Admission } from "./phase12-admission-adapter.js";
+export { buildPhase12RouteAdapter, assertPhase12RoutePlan } from "./phase12-route-adapter.js";
+export { buildPhase12ProjectionAdapter, compilePhase12DirectReportProjection } from "./phase12-projection-adapter.js";
+export { loadPhase12ReportContract, assertPhase12ReportContract } from "./phase12-report-contract.js";
