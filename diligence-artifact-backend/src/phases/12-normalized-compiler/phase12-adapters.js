c0c6def84fe021b15e4d7ae2db0b202dc4eea5cd
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-12 21:49:21 +0530

    Export CO-P12-04 profile family and validator APIs

--- diligence-artifact-backend/src/phases/12-normalized-compiler/phase12-adapters.js
+++ diligence-artifact-backend/src/phases/12-normalized-compiler/phase12-adapters.js
@@ -2,3 +2,10 @@ export { buildPhase12AdmissionAdapter, assertPhase12Admission } from "./phase12-
 export { buildPhase12RouteAdapter, assertPhase12RoutePlan } from "./phase12-route-adapter.js";
 export { buildPhase12ProjectionAdapter, compilePhase12DirectReportProjection } from "./phase12-projection-adapter.js";
 export { loadPhase12ReportContract, assertPhase12ReportContract } from "./phase12-report-contract.js";
+export { validatePhase12CompilerOutput, assertPhase12CompilerValidation } from "./phase12-compiler-validator.js";
+export {
+  CANONICAL_SECTION_ARTIFACTS,
+  REPORT_FACING_ARTIFACTS,
+  SECTION5_CHILD_PROFILES,
+  SECTION8_CHILD_PROFILES
+} from "./phase12-artifact-family.contract.js";
