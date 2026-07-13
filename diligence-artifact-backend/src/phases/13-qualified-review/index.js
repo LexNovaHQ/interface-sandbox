b3f9d4061d13392527d8f9ba28674850a62f2de3
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-13 22:37:46 +0530

    Add Phase 13 domain resolution exports

---
+++ diligence-artifact-backend/src/phases/13-qualified-review/index.js
@@ -0,0 +1,6 @@
+export * from "./phase13-authority.contract.js";
+export * from "./registry/index.js";
+export * from "./activation/qr-registry-activation-resolver.js";
+export * from "./resolution/phase12-qr-value-resolver.js";
+export * from "./ledger/qr-active-field-ledger.builder.js";
+export * from "./phase13-domain-field-resolution.builder.js";
