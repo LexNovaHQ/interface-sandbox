28cd0ae9bcf0cab7a9881f0766dd5b89d26f9d2a
LexNovaHQ <shwetabh.singh@lexnovahq.com>
2026-07-13 22:07:28 +0530

    Export Phase 13 QR authority modules

---
+++ diligence-artifact-backend/src/phases/13-qualified-review/registry/index.js
@@ -0,0 +1,13 @@
+export {
+  DEFAULT_ACTIVE_QR_POINTER_PATH,
+  DEFAULT_PHASE13_BACKEND_ROOT,
+  QR_REGISTRY_LOADER_VERSION,
+  loadQrRegistryAuthority,
+  resolveAuthorityPath
+} from "./qr-registry-loader.js";
+
+export {
+  QR_REGISTRY_STRUCTURAL_VALIDATOR_VERSION,
+  loadAndValidateQrRegistryAuthority,
+  validateQrRegistryAuthority
+} from "./qr-registry-structural-validator.js";
